d789261c12a2c4394818e353769bb9b6c5327b75
DavidTesa19 <davitesa10@seznam.cz>
2026-03-27 03:04:18 +0100

    Revert "Fix AG Grid legacy theme configuration"
    This reverts commit 5fbcd0a9d57241263ff392ebd1ac4c7510c27305.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,5 @@
-import { provideGlobalGridOptions } from "ag-grid-community";
+import "ag-grid-community/styles/ag-grid.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
-provideGlobalGridOptions({ theme: "legacy" });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
