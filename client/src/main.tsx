5fbcd0a9d57241263ff392ebd1ac4c7510c27305
DavidTesa19 <davitesa10@seznam.cz>
2026-03-27 02:49:54 +0100

    Fix AG Grid legacy theme configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
-import "ag-grid-community/styles/ag-grid.css";
+import { provideGlobalGridOptions } from "ag-grid-community";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+
+provideGlobalGridOptions({ theme: "legacy" });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
