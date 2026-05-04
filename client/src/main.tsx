5bc89e0b4a8e4dc3946a9f8d508920132de0dc24
DavidTesa19 <davitesa10@seznam.cz>
2026-05-04 22:19:41 +0200

    Force client redeploy for document explorer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+document.documentElement.dataset.appBuild = "2026-05-04-doc-explorer";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
